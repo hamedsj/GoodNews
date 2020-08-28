98d95635db918a0fa25fbb2fceb6445fad8b5fea
hamedsj <hamedsj5@gmail.com>
2020-08-28 19:25:15 +0430

    add navigation to project

--- app/src/main/java/me/pitok/goodnews/MainActivity.kt
+++ app/src/main/java/me/pitok/goodnews/MainActivity.kt
@@ -1,11 +1,30 @@
 package me.pitok.goodnews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null){
+            setupNavigation()
+        }
     }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        setupNavigation()
+    }
+
+    private fun setupNavigation(){
+        val host = NavHostFragment.create(R.navigation.navigation_graph)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.mainActivityRoot, host)
+            .setPrimaryNavigationFragment(host)
+            .commit()
+    }
+
 }
