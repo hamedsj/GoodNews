f1aadbefa520b221d72870ea42ecef9118009d3c
hamedsj <hamedsj5@gmail.com>
2020-09-01 10:46:54 +0430

    add navigating and dark mode to app

--- app/src/main/java/me/pitok/goodnews/GoodNewsApplication.kt
+++ app/src/main/java/me/pitok/goodnews/GoodNewsApplication.kt
@@ -2,11 +2,13 @@ package me.pitok.goodnews
 
 import android.app.Application
 import me.pitok.androidcore.builder.AndroidCoreComponentBuilder
+import me.pitok.settings.di.builder.SettingsComponentBuilder
 
 class GoodNewsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         AndroidCoreComponentBuilder.application = this
+        SettingsComponentBuilder.getComponent().provideStartupTask().run()
     }
 }
--- app/src/main/java/me/pitok/goodnews/MainActivity.kt
+++ app/src/main/java/me/pitok/goodnews/MainActivity.kt
@@ -1,13 +1,31 @@
 package me.pitok.goodnews
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.NavHostFragment
+import me.pitok.settings.entity.UIMode
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val NAV_HOST_TAG = "good_news_nav_host"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val userInterfaceMode = when (UIMode.currentUIMode) {
+            is UIMode.DarkMode -> AppCompatDelegate.MODE_NIGHT_YES
+            is UIMode.LightMode -> AppCompatDelegate.MODE_NIGHT_NO
+            else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        }
+        AppCompatDelegate.setDefaultNightMode(userInterfaceMode)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        overridePendingTransition(
+            R.anim.enter_activity_fade_in_anim,
+            R.anim.enter_activity_fade_out_anim
+        )
         if (savedInstanceState == null){
             setupNavigation()
         }
@@ -19,12 +37,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupNavigation(){
-        val host = NavHostFragment.create(R.navigation.navigation_graph)
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.mainActivityRoot, host)
-            .setPrimaryNavigationFragment(host)
-            .commit()
+        if (supportFragmentManager.findFragmentByTag(NAV_HOST_TAG) != null){
+            val host = supportFragmentManager
+                .findFragmentByTag(NAV_HOST_TAG) as NavHostFragment
+            supportFragmentManager
+                .beginTransaction()
+                .attach(host)
+                .setPrimaryNavigationFragment(host)
+                .commit()
+        }else{
+            val host = NavHostFragment.create(R.navigation.navigation_graph)
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.mainActivityRoot, host, NAV_HOST_TAG)
+                .setPrimaryNavigationFragment(host)
+                .commit()
+        }
     }
 
 }
